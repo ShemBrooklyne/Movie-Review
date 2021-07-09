c6ae4ec96c5bb4c32c132933c8dfc4288911325e
ShemBrooklyne <shemnyatti@gmail.com>
2021-07-09 11:03:07 +0300

    customized activities using intent anime

---
+++ app/src/main/java/org/geek/moviereview/SplashActivity.java
@@ -0,0 +1,38 @@
+package org.geek.moviereview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import maes.tech.intentanim.CustomIntent;
+
+public class SplashActivity extends AppCompatActivity {
+
+    Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+//        Action Bar hide function
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+//        Delay request
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, DiscoverActivity.class);
+                startActivity(intent);
+
+//              Activity transition animation
+                CustomIntent.customType(SplashActivity.this, "fadein-to-fadeout");
+                finish();
+            }
+        }, 2500);
+    }
+}
