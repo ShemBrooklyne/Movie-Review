9cf0409d23c170785b1d9ed2bd26e7d202636318
ShemBrooklyne <shemnyatti@gmail.com>
2021-07-16 14:53:07 +0300

    implemented collapsing toolbar layout

--- app/src/main/java/org/geek/moviereview/DetailActivity.java
+++ app/src/main/java/org/geek/moviereview/DetailActivity.java
@@ -39,16 +39,15 @@ public class DetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_detail);
+        setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
-//        activateCollapsingToolbar();
+        initCollapsingToolbar();
 
         Intent intent = getIntent();
         if (intent.hasExtra("movies")) {
@@ -79,28 +78,29 @@ public class DetailActivity extends AppCompatActivity {
 
     }
 
-//    private void activateCollapsingToolbar() {
-//        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsingToolBar);
-//        collapsingToolbarLayout.setTitle(" ");
-//        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appBar);
-//        appBarLayout.setExpanded(true);
-//
-//        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
-//            boolean isShow = false;
-//            int scrollRange = -1;
-//            @Override
-//            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
-//                if (scrollRange == -1){
-//                    scrollRange = appBarLayout.getTotalScrollRange();
-//                }
-//                if (scrollRange + verticalOffset == 0){
-//                    collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
-//                    isShow = true;
-//                }else if (isShow){
-//                    collapsingToolbarLayout.setTitle(" ");
-//                    isShow = false;
-//                }
-//            }
-//        });
-//    }
+    private void initCollapsingToolbar() {
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1){
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0){
+                    collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
+                    isShow = true;
+                }else if (isShow){
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
 }
